7b7486561cd3f17463a8e08266488ea8aa067456
Aboeleneen <aboeleneen.98@yahoo.com>
2020-07-01 09:38:07 +0200

    finish blog website challenge

--- Blog Website Challenge/app.js
+++ Blog Website Challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,9 +17,47 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+var posts = [];
 
+// home route
+app.get("/" , function(req,res){
+	res.render('home' , {homeStartingContent : homeStartingContent , posts: posts});
+});
 
+// contact route
+app.get("/contact" , function(req,res){
+	res.render('contact' , {contactContent : contactContent});
+});
 
+// about route
+app.get("/about" , function(req,res){
+	res.render('about' , {aboutContent:aboutContent})
+});
+
+// compose route
+app.get("/compose" , function(req,res){
+	res.render('compose');
+});
+
+app.post("/compose" , function(req,res){
+	const post = {
+		title : req.body.postTitle,
+		body : req.body.postBody
+	};
+	posts.push(post);	
+	res.redirect("/");
+})
+
+// posts route
+app.get("/posts/:postTitle"  , function(req,res){
+	var postTitle = lodash.lowerCase(req.params.postTitle);
+	for(var i=0;i<posts.length;i++){
+		var storedTitle = lodash.lowerCase(posts[i].title) ;
+		if(postTitle === storedTitle){
+			res.render('post' , {post:posts[i]});	
+		}
+	}
+});
 
 
 
